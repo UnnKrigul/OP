7893d3ef3c54b235fc91c96903cc6872d7e10ff8
unn.krigul <unn.krigul@khk.ee>
2019-11-11 11:26:34 +0200

    Alert/prompt/confirm

---
+++ app.js
@@ -0,0 +1,18 @@
+// Window objects / methods / properties
+
+// alert
+// window.alert('tere hommikust');
+// alert('tere hommikust');
+
+//prompt
+
+const sisend = prompt('sisesta oma nimi');
+alert('tere hommikust ' + sisend);
+
+//confirm
+if(confirm('Kas oled nõus')) {
+    console.log('jah')
+}
+else{
+    console.log('ei');
+}
