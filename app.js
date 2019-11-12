13c28d78c8cb9463607a6346eb9866767c09237e
unn.krigul <unn.krigul@khk.ee>
2019-11-12 15:49:53 +0200

    Query selectoriga stiilide muutmine

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ id = document.getElementById('title').id; // id
 klass = document.getElementById('title').className; // klass
 
 // stiili muutmine
-sisu.style.background = '#333'
-sisu.style.color = '#fff'
-sisu.style.padding = '15px'
+sisu.style.background = '#333';
+sisu.style.color = '#fff';
+sisu.style.padding = '15px';
+
+document.querySelector('li').style.color = 'red'; // leiab ESIMESE sellise elemendi
+document.querySelector('ul li'). style.color = 'blue';
+document.querySelector('li:last-child'). style.color = 'green' ;
+document.querySelector('li:nth-child(2)'). style.color = 'red' ;
+
+// sisu muutmine
+sisu.textContent = 'Minu ülesanded'; // textContent
+sisu.innerText = 'Just minu ülesanded';
+sisu.innerHTML = '<span style="color: green;">Ülesanded</span>';
+
+// query selector
+sisu = document.querySelector('h5');
+sisu = document.querySelector('#title');
+sisu = document.querySelector('.card-title');
 
 console.log(sisu);
