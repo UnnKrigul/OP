6bcb97d99d48f56de1a4fea3381b31e71ecb6fec
unn.krigul <unn.krigul@khk.ee>
2019-11-15 13:45:40 +0200

    DOM elementide omadused ja neile refereerimine

--- app.js
+++ app.js
@@ -41,3 +41,50 @@ let nimekiriPaaritu = document.querySelectorAll('li:nth-child(odd)');
 console.log(nimekiriPaaritu);
 let nimekiriPaaris = document.querySelectorAll('li:nth-child(even)');
 console.log(nimekiriPaaris);
+
+// node omadused ja meetodid
+
+let nimekiri = document.querySelectorAll('ul.collection');
+let nimekirjaElement = document.querySelectorAll('li.collection-item')
+console.log(nimekiri);
+console.log(nimekirjaElement);
+
+nimekiri = document.querySelector('ul.collection');
+console.log(nimekiri);
+
+sisu = nimekiri.childNodes;
+sisu = nimekiri.childNodes[0];
+/*
+1 - element
+2 - atripuut
+3 - tekst
+8 - kommentaar
+9 - dokument
+10 - doctype
+*/ 
+
+sisu = nimekiri.children;
+console.log(sisu);
+
+nimekiri.children[1].textContent = 'Õpi rohkem JS';
+sisu = nimekiri.children[2].children[0];
+
+
+sisu = nimekiri.firstChild; // node type
+sisu = nimekiri.firstElementChild; // value
+console.log(sisu);
+
+sisu = nimekiri.lastElementChild; // value
+console.log(sisu);
+
+sisu = nimekiri.childElementCount;
+console.log(sisu);
+
+nimekirjaElement = document.querySelector('li.collection-item');
+sisu = nimekirjaElement.parentElement;
+sisu = nimekirjaElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+
+sisu = nimekirjaElement.nextSibling; // node type
+sisu = nimekirjaElement.nextElementSibling; //value
+sisu = nimekirjaElement.previousElementSibling;
+console.log(sisu);
