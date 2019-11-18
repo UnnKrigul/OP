0d64b26325c1a1bac21b5781d86eb498f01b95bf
unn.krigul <unn.krigul@khk.ee>
2019-11-18 11:43:12 +0200

    Kustutamine

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // elementide kustutamine
 
-const listElem = document.querySelectorAll('li');
+let listElem = document.querySelectorAll('li');
 console.log(listElem);
 
 //kustuta konkreetne element
@@ -9,3 +9,21 @@ listElem[2].remove();
 
 const nimekiri = document.querySelector('.collection');
 nimekiri.removeChild(listElem[1]);
+
+// klassi/atribuudi kustutamine
+
+// class
+
+listElem = listElem[0];
+const link = listElem.children[0];
+
+let sisu = link.className;
+sisu = link.classList[1];
+link.classList.remove('secondary-content');
+link.classList.add('secondary-content');
+
+// atribuut
+sisu = link.getAttribute('href');
+link.setAttribute('title', 'Google');
+link.removeAttribute('title');
+console.log(sisu);
