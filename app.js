9d88596064c973f9eca96a13feaa0c3cfac20315
unn.krigul <unn.krigul@khk.ee>
2019-11-20 08:54:47 +0200

    Sündmused, funktsioon kutsutud sündmuse sees

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
-// elementide kustutamine
+// sündmused
 
-let listElem = document.querySelectorAll('li');
-console.log(listElem);
-
-//kustuta konkreetne element
-
-listElem[2].remove();
-
-const nimekiri = document.querySelector('.collection');
-nimekiri.removeChild(listElem[1]);
-
-// klassi/atribuudi kustutamine
-
-// class
-
-listElem = listElem[0];
-const link = listElem.children[0];
-
-let sisu = link.className;
-sisu = link.classList[1];
-link.classList.remove('secondary-content');
-link.classList.add('secondary-content');
-
-// atribuut
-sisu = link.getAttribute('href');
-link.setAttribute('title', 'Google');
-link.removeAttribute('title');
-console.log(sisu);
+const clearTasks = document.querySelector('.clear-tasks');
+clearTasks.addEventListener('click', function(e){
+    console.log('kustuta kõik');
+    // e.preventDefault();
+})
