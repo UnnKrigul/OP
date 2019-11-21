9a84fe1c2438f6aeecd9b40e7d5290cd4024c6f9
UnnKrigul <unn.krigul@khk.ee>
2019-11-21 21:02:38 +0200

    Mällu lisamine OK, Listi loome laadimisel OK

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
-// storage
-
-// salvestamine
-// localStorage.setItem('nimi', 'Unn');
-// localStorage.setItem('perenimi', 'Krigul');
-
-// väärtuse eemaldamine
-// localStorage.removeItem('nimi');
-
-// väärtuste lugemine
-// const perenimi = localStorage.getItem('perenimi');
-// const nimi = localStorage.getItem('nimi');
-
-// console.log(nimi+perenimi);
-
-// puhasta kõik andmed
-
-// localStorage.clear()
-
-document.querySelector('form').addEventListener('submit', salvesta);
-
+// Mällu salvestamine
+const form = document.querySelector('form');
+let list;
+form.addEventListener('submit', salvesta)
 function salvesta(e){
-    const value = document.getElementById('task').value;
-    let ylesanded;
+    const input = document.querySelector('#task').value;
+    ;
     if(localStorage.getItem('tasks') === null){
-        ylesanded = [];
-        localStorage.setItem('tasks', JSON.stringify(ylesanded));
-        console.log('ylesanded');
+        list = [];
+        localStorage.setItem('tasks', JSON.stringify(list));
     }
-    else {
-        console.log('tuleb lugeda');
-        ylesanded = JSON.parse(localStorage.getItem('tasks'));
+    else{
+        list = JSON.parse(localStorage.getItem('tasks'));
     }
-    ylesanded.push(value);
-    console.log(ylesanded);
-    localStorage.setItem('tasks', JSON.stringify(ylesanded))
-};
+    list.push(input)
+    console.log(list);
+    localStorage.setItem('tasks', JSON.stringify(list));
+}
+
+// Mälust li loomine ja dokumenti paigutamine
+let listItems = JSON.parse(localStorage.getItem('tasks'));
+
+listItems.forEach(function (e, i){
+    // loon li elemendi
+    const li = document.createElement('li');
+    li.innerText = e;
+    li.className = 'collection-item';
+    document.querySelector('.collection').appendChild(li);
+    // lisan kustutamise nupu
+    const del = document.createElement('a');
+    del.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+    del.className = 'secondary-content';
+    del.href = '#';
+    li.appendChild(del);
+    // kustutamise nupu funktsionaalsus
+    del.addEventListener('click', rm);
+    function rm(){
+        console.log(i);
+        listItems.splice(i, 1);
+        li.remove();
+        localStorage.setItem('tasks', JSON.stringify(listItems))
+    };
+});
+
+// Kustuta kõik nupp
+const clear = document.querySelector('.clear-tasks');
+clear.addEventListener('click', clearMem);
+function clearMem(e){
+    localStorage.clear();
+}
